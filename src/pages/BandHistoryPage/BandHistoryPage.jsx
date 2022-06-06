77a2fbd5e216e405584792330afe2ce86aa1c6f0
flyingisfreedom5 <berkay_tor@hotmail.com>
2022-06-06 13:44:31 +0300

    mongodb server fixed commit

--- src/pages/BandHistoryPage/BandHistoryPage.jsx
+++ src/pages/BandHistoryPage/BandHistoryPage.jsx
@@ -1,6 +1,6 @@
 import { checkToken } from "../../utilities/users-service";
 
-export default function OrderHistoryPage() {
+export default function BandHistoryPage() {
   async function handleCheckToken() {
     const expDate = await checkToken();
     console.log(expDate);
@@ -8,7 +8,7 @@ export default function OrderHistoryPage() {
   
   return (
     <>
-      <h1>OrderHistoryPage</h1>
+      <h1>BandHistoryPage</h1>
       <button onClick={handleCheckToken}>Check When My Login Expires</button>
     </>
   );
---
+++ src/pages/NewBandPage/NewBandPage.jsx
@@ -0,0 +1,5 @@
+export default function NewBandPage() {
+  return (
+    <h1>NewBandPage</h1>
+  );
+}
